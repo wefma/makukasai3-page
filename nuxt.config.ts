3ff60bbb9b2fef5cc4e7730f8672fd09b1054972
wefmaika1985@gmail.com <wefmaika1985@gmail.com>
2025-11-13 23:09:52 +0900

    seo対策

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,9 @@ export default defineNuxtConfig({
   appConfig: {
     header: {
       logo: {
-        light: '/logo.png',
-        dark: '/logo.png',
-        alt: 'Site logo',
+        light: "/logo.png",
+        dark: "/logo.png",
+        alt: "Site logo",
       },
     },
   },
@@ -14,8 +14,14 @@ export default defineNuxtConfig({
       sqliteConnector: "native",
     },
   },
-  modules: ["@nuxt/content", "@nuxt/eslint"],
-  css: ['~/assets/css/theme.css'],
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxtjs/seo"],
+  site: {
+    url: "https://makukasai3.wefma.net",
+    name: "幕華祭永夜篇攻略",
+    defaultLocale: "ja",
+    indexable: true,
+  },
+  css: ["~/assets/css/theme.css"],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
